c50ba1a281972f0d5a394e56a776024347ca2887
beepscore <support@beepscore.com>
2013-06-20 23:18:45 -0700

    In test_sorter.py move test_datas into method test_heapify()

--- test_sorter.py
+++ test_sorter.py
@@ -5,26 +5,26 @@ import unittest
 
 class TestSorter(unittest.TestCase):
 
-    almost_heap_index = 0
-    expected_result_index = 1
-
-    test_datas = [
-        [[5], [5]],
-        [[2, 3, 1], [3, 2, 1]],
-    ]
-
     def setUp(self):
         pass
 
     def test_heapify(self):
 
-        for test_data in self.test_datas:
+        almost_heap_index = 0
+        expected_result_index = 1
+
+        test_datas = [
+            [[5], [5]],
+            [[2, 3, 1], [3, 2, 1]],
+        ]
+
+        for test_data in test_datas:
             # module sorter, class Sorter, method heapify()
-            result = sorter.Sorter.heapify(test_data[self.almost_heap_index])
-            self.assertEqual(test_data[self.expected_result_index],
+            result = sorter.Sorter.heapify(test_data[almost_heap_index])
+            self.assertEqual(test_data[expected_result_index],
                             result,
-                            'heapify({}) expected {} but got {}'.format(test_data[self.almost_heap_index],
-                                                                        test_data[self.expected_result_index],
+                            'heapify({}) expected {} but got {}'.format(test_data[almost_heap_index],
+                                                                        test_data[expected_result_index],
                                                                         result))
 
 if __name__ == "__main__": unittest.main()
