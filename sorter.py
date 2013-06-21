564c2caa3559f490a20481f9826363e026ed6632
beepscore <support@beepscore.com>
2013-06-20 22:39:02 -0700

    Add sorter.py. Add simplest code to make test pass.
    To run tests, $ python3 test_sorter.py

---
+++ sorter.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+class Sorter():
+    '''
+    Sort a list using heap sort.
+    '''
+
+    def __init__(self):
+        pass
+
+    def heapify(almost_max_heap):
+        # write simplest code that will pass test
+        return [3, 2, 1]
+
